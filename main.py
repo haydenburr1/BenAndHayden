8b3845796a25be51d5377a441826e01a21360965
haydenburr1 <155483992+haydenburr1@users.noreply.github.com>
2024-12-10 12:00:30 +0000

    starting files

--- main.py
+++ main.py
@@ -1,7 +1,32 @@
 import pygame as pg 
+from settings import *
+import sys
 
 class App:
     def __init__(self):
+        pg.init()
+        self.screen = pg.display.set_mode(RES)
+        self.clock = pg.time.Clock()
+      
+    def game_loop(self):
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                pg.quit()
+                sys.exit()
+
+    def draw(self):
         pass
     
+    def update(self):
+        pg.display.update()
+        self.clock.tick(FPS)
+        
+    def run(self):
+        while True:
+            self.game_loop()
+            self.draw()
+            self.update()
     
+if __name__ == "__main__":
+    app = App()
+    app.run()
